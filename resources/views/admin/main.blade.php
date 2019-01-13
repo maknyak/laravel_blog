6257ff201fe8eec7290d6fd1ff0c89788daeb86f
Ian Maulana <ian_maulana@yahoo.com>
2019-01-13 23:28:40 +0700

    remove active menu

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -17,8 +17,8 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Dashboard <span class="sr-only">(current)</span></a>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Dashboard</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="{{ url('post') }}">Post</a>
